d674acf83c5d88ffea559c36c2e6a30a35a4703c
Alexey Noskov <alexey.noskov@gmail.com>
2017-01-31 21:51:30 +0300

    Using he_uniform init

--- train.py
+++ train.py
@@ -100,40 +100,40 @@ def combined_loss(y_true, y_pred):
 
 def get_unet():
     inputs = Input((3, image_patch_size, image_patch_size))
-    conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
-    conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv1)
+    conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same', init='he_uniform')(inputs)
+    conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same', init='he_uniform')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-    conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool1)
-    conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv2)
+    conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same', init='he_uniform')(pool1)
+    conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same', init='he_uniform')(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(pool2)
-    conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv3)
+    conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same', init='he_uniform')(pool2)
+    conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same', init='he_uniform')(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-    conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(pool3)
-    conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv4)
+    conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same', init='he_uniform')(pool3)
+    conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same', init='he_uniform')(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-    conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(pool4)
-    conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(conv5)
+    conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same', init='he_uniform')(pool4)
+    conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same', init='he_uniform')(conv5)
 
     up6 = merge([UpSampling2D(size=(2, 2))(conv5), conv4], mode='concat', concat_axis=1)
-    conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(up6)
-    conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv6)
+    conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same', init='he_uniform')(up6)
+    conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same', init='he_uniform')(conv6)
 
     up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv3], mode='concat', concat_axis=1)
-    conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(up7)
-    conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv7)
+    conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same', init='he_uniform')(up7)
+    conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same', init='he_uniform')(conv7)
 
     up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv2], mode='concat', concat_axis=1)
-    conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(up8)
-    conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv8)
+    conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same', init='he_uniform')(up8)
+    conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same', init='he_uniform')(conv8)
 
     up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv1], mode='concat', concat_axis=1)
-    conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up9)
-    conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv9)
+    conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same', init='he_uniform')(up9)
+    conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same', init='he_uniform')(conv9)
 
     conv10 = Convolution2D(n_classes, 1, 1, activation='sigmoid')(conv9)
 
