4673be53d2e44d65ded5edece156fbd33e20a629
Alexey Noskov <alexey.noskov@gmail.com>
2017-02-26 19:02:51 +0300

    Smooth masks

--- prepare-masks.py
+++ prepare-masks.py
@@ -7,6 +7,8 @@ import numpy as np
 
 import shapely.wkt as wkt
 
+import cv2
+
 
 print "Preparing train image masks..."
 
@@ -19,11 +21,13 @@ for image_id, image_cls_wkt in train_wkt.groupby('image_id'):
 
     meta = load_pickle('cache/meta/%s.pickle' % image_id)
 
-    mask = np.zeros((n_classes, meta['shape'][1], meta['shape'][2]), dtype=np.uint8)
+    mask = np.zeros((n_classes, meta['shape'][1], meta['shape'][2]), dtype=np.float32)
 
     for tp in image_cls_wkt.itertuples():
         poly = wkt.loads(tp.multi_poly_wkt)
-        mask[tp.cls-1] = poly_to_mask(poly, meta['shape'][1:], [xmax, ymin])
+        h, w = meta['shape'][1:]
+
+        mask[tp.cls-1] = np.clip(cv2.resize(poly_to_mask(poly, (w * 4, h * 4), [xmax, ymin]).astype(np.float32), (w, h), interpolation=cv2.INTER_AREA), 0, 1)
 
     np.save('cache/masks/%s.npy' % image_id, mask)
 
